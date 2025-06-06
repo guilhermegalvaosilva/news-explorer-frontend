6983a965346698f1d3366bc5d6835b8a8e4492e9
Guilherme Silva <gui.galvaosimplicio@gmail.com>
2025-06-06 21:21:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/news-explorer-frontend/', // nome do repositório
+  base: "/news-explorer-frontend/", // nome do repositório no GitHub
   plugins: [react()],
-})
+});
