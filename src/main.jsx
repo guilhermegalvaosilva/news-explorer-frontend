4f6d5b46e3279513baa50570d61357aab6af73a9
Guilherme Silva <gui.galvaosimplicio@gmail.com>
2025-06-06 19:37:39 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
-import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <Router>
-    <StrictMode>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </StrictMode>
-  </Router>,
-);
+    </BrowserRouter>
+  </React.StrictMode>
+)
